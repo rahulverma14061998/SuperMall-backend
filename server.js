a91c6c172452c5bdebcef00311909de07b3f6f14
Rahul Verma <rahulverma14061998@gmail.com>
2024-12-12 14:20:01 +0530

    Initial commit for SuperMall project

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const Razorpay = require("razorpay");
 
 const shopRoutes = require("./routes/shopRoutes");
 const authRoutes = require("./routes/authRoutes");
@@ -20,28 +19,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Razorpay
-const razorpay = new Razorpay({
-  key_id: "YOUR_RAZORPAY_KEY_ID",
-  key_secret: "YOUR_RAZORPAY_KEY_SECRET",
-});
-
-app.post("/create-order", async (req, res) => {
-  const { amount } = req.body; // Amount in paise (e.g., ₹100 = 10000)
-
-  try {
-    const options = {
-      amount: amount,
-      currency: "INR",
-      receipt: "order_rcptid_11",
-    };
-    const order = await razorpay.orders.create(options);
-    res.status(200).json(order);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
 // Use the shop routes
 app.use("/api/shops", shopRoutes);
 app.use("/api/products", productRoutes);
@@ -56,4 +33,4 @@ mongoose
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT)
+app.listen(PORT);
